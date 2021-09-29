2e30b9fd417e8b5d2eb2931cfccd39e9de338a20
Ismael <albertismael7@gmail.com>
2021-09-29 14:13:58 -0300

    up

--- pages/mountains/_slug.vue
+++ pages/mountains/_slug.vue
@@ -1,3 +1,16 @@
-<template></template>
-<style scoped>
-</style>
+<template>
+  <div>
+    <h1>Continent: {{}}</h1>
+    <h2>Mountain: {{}}</h2>
+    <p>Path: {{ $route.path }}</p>
+    <NuxtLink to="/">voltar inicio</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const mountains = await $axios.$get("https://api.nuxtjs.dev/mountains");
+    console.log(mountains);
+  },
+};
+</script>
